aa27090b1b710b4bb759294c496e1602983887f9
giabao110 <giabaofotography@gmail.com>
2020-08-16 14:49:02 +0700

    Initial Commit

---
+++ MessChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+// ViewController.swift
+// MessChat
+//
+// Created by GIABAO Photography on 8/16/20.
+// Copyright © 2020 GIABAO Photography. All rights reserved.
+// VAN HIEN University 
+//
+// Website: https://giabaophoto.com
+// 
+// 
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
